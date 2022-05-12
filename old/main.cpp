a0b72f9fda833c0a85034cf724a204358e624b04
TerrificTable <85793326+TerrificTable@users.noreply.github.com>
2022-05-12 08:47:04 +0200

    Create main.cpp

---
+++ old/main.cpp
@@ -0,0 +1,31 @@
+
+#include "screen.h"
+#include "wlan_leds.h"
+#include "scd30.h"
+
+
+
+
+void setup() {
+    Wire.begin();
+    Serial.begin(115200);
+    delay(100);
+    
+    Serial.println("\nBooting...");
+
+    display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+    display.clearDisplay();
+    display.display();
+
+    DisplayText("\nStarting...");
+}
+
+
+void loop(){
+    WLAN();
+    MQTT(); 
+
+    LED(5, CRGB::Green, 30);
+
+    SCD30();
+}
